1441c44e5775af7de2227c4fe84131c8dcbd9b5f
bcalm-2 <dwivedisrashti6@gmail.com>
2024-01-14 13:33:34 +0530

    Solved climbing stairs problem with fibo approach

--- dp/climbingStairs.js
+++ dp/climbingStairs.js
@@ -1,13 +1,12 @@
 var climbStairs = function (n) {
-    if (n === 2 || n === 1) {
-        return n;
+    if (n === 0 || n === 1) {
+        return 1;
     }
-    const dp = [];
-    dp[0] = 1;
-    dp[1] = 2;
+    let prev = 1, curr = 1;
     for (let i = 2; i <= n; i++) {
         const temp = curr;
-        dp[i] = dp[i - 1] + dp[i - 2]
+        curr = prev + curr;
+        prev = temp;
     }
-    return dp[n - 1];
+    return curr;
 };
